3079d3d9f1c8303b350da0a35142e030ea35d924
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-06-02 12:25:12 +0100

    added the result page

---
+++ lib/result_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RESULT PAGE'),
+      ),
+      body: const Text('RESULT PAGE'),
+    );
+  }
+}
